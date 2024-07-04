31b3154774056924eb19d46756ba3e18d264618a
shantanuFyndGit <shantanuradke@fynd-external.com>
2024-07-04 16:35:30 +0530

    minor fix

--- constants.js
+++ constants.js
@@ -3,6 +3,6 @@ const packageInfo = require("./package.json");
 const integrationVersion = packageInfo.version;
 const zapierCoreVersion = packageInfo.dependencies["zapier-platform-core"];
 
-const INTEGRATION_PLATFORM = `Pixelbinio/${integrationVersion} (Zapier/${zapierCoreVersion})`;
+const INTEGRATION_PLATFORM = `PixelbinioIntegration/${integrationVersion} (Zapier/${zapierCoreVersion})`;
 
 module.exports = INTEGRATION_PLATFORM;
