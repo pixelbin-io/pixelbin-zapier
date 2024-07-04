fedbae08cc44634e701e0655e1bf37357a00da56
shantanuFyndGit <shantanuradke@fynd-external.com>
2024-07-04 10:49:09 +0530

    minor fix

--- constants.js
+++ constants.js
@@ -1,4 +1,3 @@
 const INTEGRATION_PLATFORM = "PixelbinPixelbinIo/1.0.3 (Figma/116.1.3)";
 
-module.exports.INTEGRATION_PLATFORM =
-	"PixelbinPixelbinIo/1.0.3 (Figma/116.1.3)";
+module.exports = INTEGRATION_PLATFORM;
