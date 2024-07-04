8b8cb662b4cee7daffbbbcf554c6e2884e8bce5c
shantanuFyndGit <shantanuradke@fynd-external.com>
2024-07-04 11:24:04 +0530

    minor changes

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-const INTEGRATION_PLATFORM = "PixelbinPixelbinIo/1.0.3 (Figma/116.1.3)";
+const INTEGRATION_PLATFORM = "PixelbinPixelbinIo/1.0.3 (Zapier/116.1.3)";
 
 module.exports = INTEGRATION_PLATFORM;
